d387c2451b48079e59ce0ae89413e2320bcd9749
ddenizer <ddenizer14@gmail.com>
2022-08-13 22:53:27 +0300

    #4 TODO'S

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// TODO CREATE CONTROLLERS AND ACTIONS
+// TODO CREATE API
